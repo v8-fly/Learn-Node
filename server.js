bf2d2a76a855093fa4e6bb7e0534ad6031202e4d
v8-fly <98163507+v8-fly@users.noreply.github.com>
2022-09-11 15:20:33 +0530

    using extension name

--- server.js
+++ server.js
@@ -13,32 +13,59 @@ const emitter = new MyEmitter()
 
 const PORT = process.env.PORT || 3500
 
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const data = await fsPromises.readFile(filePath, "utf8")
+    response.writeHead(200, {
+      "Content-type": contentType,
+    })
+    response.end(data)
+  } catch (err) {
+    console.log(err)
+    response.statusCode = 500
+    response.end()
+  }
+}
+
 const server = http.createServer((req, res) => {
+  console.log("test", req.url, req.method)
   let filePath
-
-  switch (req.url) {
-    case "/":
-      res.statusCode = 200
-      filePath = path.join(__dirname, "views", "index.html")
-      fs.readFile(filePath, "utf8", (err, data) => {
-        res.end(data)
-      })
+  const extension = path.extname(req.url)
+  console.log("extension", extension)
+  let contentType
+  switch (extension) {
+    case ".css":
+      contentType = "text/css"
       break
-
-    // default:
-    //   break;
+    case ".js":
+      contentType = "text/javascript"
+      break
+    case ".json":
+      contentType = "application/json"
+      break
+    case ".jpg":
+      contentType = "image/jpeg"
+      break
+    case ".png":
+      contentType = "image/png"
+      break
+    case ".txt":
+      contentType = "text/plain"
+      break
+    default:
+      contentType = "text/html"
   }
 
-  // if (req.url === "/") {
-  //   res.writeHead(200, {
-  //     "Content-type": "text/html",
-  //   })
-  //   filePath = path.join(__dirname, "views", "index.html")
-  //   console.log("test", filePath)
-  //   fs.readFile(filePath, "utf8", (err, data) => {
-  //     res.end(data)
-  //   })
-  // }
+  if (contentType === "text/html" && req.url === "/") {
+    filePath = path.join(__dirname, "views", "index.html")
+  } else if (contentType === "text/css") {
+    filePath = path.join(__dirname, req.url)
+  }
+
+  const fileExists = fs.existsSync(filePath)
+  if (fileExists) {
+    serveFile(filePath, contentType, res)
+  }
 })
 
 server.listen(PORT, () => console.log(`server running on port ${PORT}`))
