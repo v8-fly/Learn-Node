e07a9421fea8068b4f9d94569bb8b92693133611
v8-fly <98163507+v8-fly@users.noreply.github.com>
2022-09-11 14:44:56 +0530

    send HTML page to client

--- server.js
+++ server.js
@@ -1,91 +1,39 @@
-const os = require("os")
+const http = require("http")
 const path = require("path")
-const { add } = require("./math")
+const fs = require("fs")
 const fsPromises = require("fs").promises
-// const file = require("./files/starter.txt")
 
-// console.log(os.type())
-// console.log(os.version())
-// console.log(os.homedir())
-// console.log(__dirname)
-// console.log(__filename)
-// console.log(path.dirname(__filename))
-// console.log(path.basename(__filename))
-// console.log(path.extname(__filename))
+const logEvents = require("./logEvents")
+const EventEmitter = require("events")
 
-// console.log(path.parse(__filename))
-// console.log(add(1, 2))
+class MyEmitter extends EventEmitter {}
 
-// console.log(__dirname)
+//initialize object
+const emitter = new MyEmitter()
 
-// fs.readFile(
-//   path.join(__dirname, "files", "starter.txt"),
-//   "utf-8",
-//   (err, data) => {
-//     if (err) {
-//       throw err
-//     } else {
-//       console.log(data)
-//     }
-//   }
-// )
+const PORT = process.env.PORT || 3500
 
-// process.on("uncaughtException", (err) => {
-//   console.error("hey HARDIK there is an uncought exception", err)
-//   process.exit(1)
-// })
-
-// fs.writeFile(
-//   path.join(__dirname, "files", "reply.txt"),
-//   "I am in Space step 1 done",
-//   (err) => {
-//     if (err) {
-//       throw err
-//     } else {
-//       fs.appendFile(
-//         path.join(__dirname, "files", "reply.txt"),
-//         "\n\n Stepp >> 2",
-//         (err) => {
-//           if (err) {
-//             throw err
-//           } else {
-//             fs.rename(
-//               path.join(__dirname, "files", "reply.txt"),
-//               path.join(__dirname, "files", "This is renamed.txt"),
-//               (err) => {
-//                 if (err) {
-//                   throw err
-//                 } else {
-//                   console.log("Rocket in Orbit lknlknln")
-//                 }
-//               }
-//             )
-//           }
-//         }
-//       )
-//       console.log("Rocket in Orbit")
-//     }
-//   }
-// )
-
-const fileOps = async () => {
-  try {
-    const data = await fsPromises.readFile(
-      path.join(__dirname, "files", "lorem.txt"),
-      "utf-8"
-    )
-    await fsPromises.appendFile(
-      path.join(__dirname, "files", "lorem.txt"),
-      `\n\n${data + "hey We wrote to the file HARDIK"}`
-    )
-    const data2 = await fsPromises.readFile(
-      path.join(__dirname, "files", "lorem.txt"),
-      "utf-8"
-    )
-    fsPromises.unlink(path.join(__dirname, "files", "lorem.txt"))
-    console.log(data2)
-  } catch (error) {
-    console.log(error)
+const server = http.createServer((req, res) => {
+  let filePath
+  if (req.url === "/") {
+    res.writeHead(200, {
+      "Content-type": "text/html",
+    })
+    filePath = path.join(__dirname, "views", "index.html")
+    console.log("test", filePath)
+    fs.readFile(filePath, "utf8", (err, data) => {
+      res.end(data)
+    })
   }
-}
-fileOps()
+  // console.log("URL is", req.url, "METHOD is", req.method)
+})
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`))
+
+// //add emiter for events
+// emitter.on("log", (msg) => logEvents(msg))
+
+// setTimeout(() => {
+//   //Emit event
+//   emitter.emit("log", "Test event Emited!!!")
+// }, 2000)
